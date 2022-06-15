66324d5a823d153b492905a94ecc541f369178a0
elogeek <elodiechristin@gmail.com>
2022-06-15 15:43:20 +0200

    course exercise react & electron (manage the data)

--- main/storage.js
+++ main/storage.js
@@ -1,4 +1,5 @@
 const ElectronStorage = require('electron-store');
+const {key} = require("wait-on/exampleConfig");
 
 class ipsStore {
 
@@ -13,5 +14,18 @@ class ipsStore {
             this.storage.set(key, value);
             return this.storage.get(key) !== undefined;
         });
+
+        // Get value
+        this.ipcMain.handle("storage-get", (event, key) => {
+            return this.storage.get(key);
+        })
+
+        // Delete value
+        this.ipcMain.handle("storage-delete", (event, key) => {
+            this.storage.delete(key);
+            return this.storage.get(key) === undefined;
+        });
     }
 }
+
+module.exports = ipsStore;
